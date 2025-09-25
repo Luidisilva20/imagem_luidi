0502b09f9bcf5dbdf3a013470394464d10b1d2dc
Luidi Carvalho Silva <luidisilva2005@icloud.com>
2025-09-25 10:00:13 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="package_luidi",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Luidi_Carvalho",
+    author_email="luidisilva2005@icloud.com",
+    description="Minha primeira imagem",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Luidisilva20/simple-package-template"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
