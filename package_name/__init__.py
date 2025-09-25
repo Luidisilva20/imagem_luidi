45af808b9ecd59e37afa4b96fec713d11d2bc7c7
Luidi Carvalho Silva <luidisilva2005@icloud.com>
2025-09-25 11:56:42 -0300

    Update __init__.py

---
+++ package_name/__init__.py
@@ -0,0 +1,11 @@
+# package_luidi/__init__.py
+
+from .file1_name import criar_imagem_praia
+from .file2_name import criar_imagem_cidade
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "criar_imagem_praia",
+    "criar_imagem_cidade"
+]
